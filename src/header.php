5319493de2f691dd97065d64a736d576fa85d994
azizul987 <wawanajah954@gmail.com>
2026-02-14 11:28:46 +0700

    first commit

---
+++ src/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="icon" href="data;,">
+</head>
+<body>
+    <div>
+         <ol style="display: flex; list-style: none; padding: 0; margin: 0; gap: 10px; margin-bottom: 20px;">
+            <li><img src=""></li>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="ruangan.php">Ruangan</a></li>
+            <li><a href="ruangan.php">Status Ruangan</a></li>
+            <li><a href="ruangan.php">Peminjaman</a></li>
+            <li><a href="ruangan.php">Login</a></li>
+        </ol>
+    </div>
+</body>
+</html>
---
+++ src/index.php
@@ -0,0 +1,15 @@
+<?php
+require __DIR__.'/header.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+     <link rel="icon" href="data;,">
+</head>
+<body>
+    wkkwkw
+</body>
+</html>
